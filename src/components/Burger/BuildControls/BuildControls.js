536f71885e07906f7b419cf3bef15b1f38fdfcf4
Ziad Eldesouky <Ziad.dessoki@yahoo.com>
2020-03-31 02:51:33 -0400

    adding controls

--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -1,5 +1,12 @@
 import React from 'react';
 
+const controls=[
+    {label:'Salad', type:'salad'},
+    {label:'Bacon',type:'bacon'},
+    {label:'Cheese',type:'cheese'},
+    {label:'Meat',type:'meat'},
+]
+
 const buildControls = (props)=>(
     <div>
         
