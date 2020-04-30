d9ef7a7403a79ca5f1d1c2e63db5a4db082f136e
Ziad Eldesouky <ziad.dessoki@yahoo.com>
2020-04-30 01:47:26 -0400

    form validation

--- src/components/UI/Input/Input.js
+++ src/components/UI/Input/Input.js
@@ -3,22 +3,29 @@ import classes from "./Input.css"
 
 const input =(props)=>{
        let inputElement = null;
+       const inputClasses = [classes.InputElement]
+       
+if(props.invalid){
+    inputClasses.push(classes.invalid)
+}
+
+
     switch(props.elementType){
         case('input'): 
             inputElement = <input 
-                            className={classes.InputElement}
+                            className={inputClasses.join(' ')}
                             {...props.elementConfig} value={props.value}
                             onChange={props.changed}/>
         break;
         case('textarea'): 
             inputElement = <textarea 
-                            className={classes.InputElement}
+                            className={inputClasses.join(' ')}
                             {...props.elementConfig} value={props.value}
                             onChange={props.changed}/>
         break;
         case('select'): 
             inputElement =( <select
-                            className={classes.InputElement}
+                            className={inputClasses.join(' ')}
                             value={props.value}
                             onChange={props.changed}>
                                 {props.elementConfig.options.map(option =>(
@@ -28,7 +35,7 @@ const input =(props)=>{
         break;
         default:
              inputElement = <input 
-                            className={classes.InputElement}
+                            className={inputClasses.join(' ')}
                             {...props.elementConfig} value={props.value}
                             onChange={props.changed}/>
         
