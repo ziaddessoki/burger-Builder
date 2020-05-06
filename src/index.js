84f85bd0a5593d33952443b0785be18f1ab7f00b
Ziad Eldesouky <ziad.dessoki@yahoo.com>
2020-05-06 12:56:09 -0400

    adding redux

--- src/index.js
+++ src/index.js
@@ -1,13 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {createStore} from 'redux'
+import {Provider} from 'react-redux'
+import reducer from './store/reducer.js'
+
 import './index.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 
+const store = createStore(reducer)
+
 const app =(
-    <BrowserRouter>
-    <App/>
-    </BrowserRouter>
+    <Provider store={store}>
+        <BrowserRouter>
+            <App/>
+        </BrowserRouter>
+    </Provider>
 )
     
 
---
+++ src/store/reducer.js
@@ -0,0 +1,25 @@
+import * as actionTypes from './actions'
+
+initialState={
+    ingredients:null,
+    totalPrice: 4,
+}
+
+const reducer =(state= initialState , action)=>{
+    switch(action.type){
+        case actionTypes.ADD_INGREDIENT:
+            return{
+
+            };
+        case actionTypes.REMOVE_INGREDIENT:
+            return{
+
+            } 
+            default:
+                return state;  
+    };
+    
+}       
+
+
+export default reducer
