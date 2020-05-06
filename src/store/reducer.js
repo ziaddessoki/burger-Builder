782b80dff731f034c24f72c84d1db202a668ec88
Ziad Eldesouky <ziad.dessoki@yahoo.com>
2020-05-06 14:43:14 -0400

    adding redux

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,7 +1,13 @@
 import * as actionTypes from './actions'
 
 initialState={
-    ingredients:null,
+    // we gettting it from BD on the old state but lets keep it like ths for now
+    ingredients:{
+        bacon:0,
+        salad:0,
+        cheese:0,
+        meat:0
+    },
     totalPrice: 4,
 }
 
@@ -9,11 +15,19 @@ const reducer =(state= initialState , action)=>{
     switch(action.type){
         case actionTypes.ADD_INGREDIENT:
             return{
-
+                ...state,
+                ingredients:{
+                    ...state.ingredients,
+                    [action.ingredientName]:state.ingredients[action.ingredientName]+1
+                }
             };
         case actionTypes.REMOVE_INGREDIENT:
             return{
-
+                ...state,
+                ingredients:{
+                    ...state.ingredients,
+                    [action.ingredientName]:state.ingredients[action.ingredientName]-1
+                }
             } 
             default:
                 return state;  
