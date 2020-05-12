6c3d3c89956512ccfec1c7c08555f7a0b00211d9
Ziad Eldesouky <ziad.dessoki@yahoo.com>
2020-05-12 11:37:00 -0400

    user auth.

--- src/container/Auth/Auth.js
+++ src/container/Auth/Auth.js
@@ -37,10 +37,31 @@ class Auth extends Component{
     }
 
     render(){
+        const formElementsArray=[]
+        for (let key in this.state.orderForm){
+            formElementsArray.push({
+                id : key,
+                config : this.state.orderForm[key]
+            })
+           
+        }
+        const form = formElementsArray.map(formElement =>(
+            <Input
+             key={formElement.id}
+             valueType={formElement.id}
+                    elementType={formElement.config.elementType} 
+                    elementConfig={formElement.config.elementConfig}
+                    value={formElement.value}
+                    invalid ={!formElement.config.valid}
+                    shouldValidate={formElement.config.validation}
+                    touched={formElement.config.touched}
+                    changed={(event)=>this.inputChangeHandler(event,formElement.id)}/>
+        ))
         return(
             <div>
                 <form>
-
+                    {form}
+                    <Button btnType="Success">Submit</Button>
                 </form>
             </div>
         )
