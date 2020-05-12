d55cb2874d351c33c4b57a16528e7d13b158486e
Ziad Eldesouky <ziad.dessoki@yahoo.com>
2020-05-12 12:48:29 -0400

    user auth.

--- src/container/Auth/Auth.js
+++ src/container/Auth/Auth.js
@@ -37,6 +37,34 @@ class Auth extends Component{
         }
     }
 
+    checkValidity(value,rules){
+        let isValid = true;
+        // for elements w/o validation
+        if(!rules){
+            return true;
+        }
+        // if the input is required, change isValid to Ture if it's not equal to an empty string
+        if(rules.required){
+            isValid = value.trim() !=='' && isValid    
+        }
+        if(rules.minLength){
+            isValid = value.length >= rules.minLength && isValid  
+        }
+        if(rules.maxLength){
+            isValid = value.length <= rules.maxLength && isValid  
+        }
+        if (rules.isEmail) {
+            const pattern = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/;
+            isValid = pattern.test(value) && isValid
+        }
+
+        if (rules.isNumeric) {
+            const pattern = /^\d+$/;
+            isValid = pattern.test(value) && isValid
+        }
+        return isValid;
+    }
+
     render(){
         const formElementsArray=[]
         for (let key in this.state.controls){
