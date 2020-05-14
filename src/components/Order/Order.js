e64b60a4fb709e9cd17626ce4098462aa2f3b1bb
Ziad Eldesouky <ziad.dessoki@yahoo.com>
2020-05-14 07:30:59 -0400

    advanced Redux

--- src/components/Order/Order.js
+++ src/components/Order/Order.js
@@ -25,7 +25,7 @@ const order = (props) => {
     return(
         <div className={classes.Order}>
             <p>Ingredients:{ingredientOutput}</p>
-            <p>Price:<strong>${Number.parseFloat(props.price)}</strong>  </p>
+            <p>Price:<strong>${Number.parseFloat(props.price).toFixed( 2 )}</strong>  </p>
         </div>
     )
     
