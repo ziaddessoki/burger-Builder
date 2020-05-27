8f67db9996961c5ebdf13a098e090cd1ef57774d
Ziad Eldesouky <ziad.dessoki@yahoo.com>
2020-05-26 23:48:05 -0400

    logout

--- src/store/reducers/auth.js
+++ src/store/reducers/auth.js
@@ -6,6 +6,7 @@ const initialState ={
     userId: null,
     error: null,
     loading: false,
+    authRedirectPath:'/'
 };
 
 const authSuccess = (state, action)=>{
@@ -29,6 +30,9 @@ const authLogout = (state,action) =>{
     return updateObject(state,{token:null, userId:null})
 }
 
+const setAuthRedirectPath = (state, actiom) =>{
+    return updateObject (state,{authRedirectPath:action.path})
+}
 
 
 // both ways work either authStart or AuthFail/sucsses, its just a cleaner code  
@@ -39,7 +43,8 @@ const reducer = (state = initialState, action) =>{
             return updateObject(state,{error:null, loading:true});
         case actionTypes.AUTH_SUCCESS: return authSuccess(state,action);
         case actionTypes.AUTH_FAIL: return authFail(state,action);
-        case actionTypes.AUTH_LOGOUT: return authLogout(state,action)
+        case actionTypes.AUTH_LOGOUT: return authLogout(state,action);
+        case actionTypes.SET_AUTH_REDIRECT_PATH : return setAuthRedirectPath(state,action)
 
         default: 
             return state;    
