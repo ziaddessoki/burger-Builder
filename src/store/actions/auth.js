6034012fe6e15706749dc372161fec98d54129e2
Ziad Eldesouky <ziad.dessoki@yahoo.com>
2020-05-30 12:16:58 -0400

    fixing logout w/refresh

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -25,6 +25,7 @@ export const authFail = (error) =>{
 export const logout = () =>{
     localStorage.removeItem('token');
     localStorage.removeItem('expirationDate');
+    localStorage.removeItem('userId');
     return{
         type: actionTypes.AUTH_LOGOUT
     }
@@ -57,6 +58,7 @@ export const auth = (email, password, isSignup) =>{
             localStorage.setItem('token',res.data.idToken);
             const expirationDate =new Date(new Date().getTime() + res.data.expiresIn * 1000);
             localStorage.setItem('expirationDate',expirationDate);
+            localStorage.setItem('userId',res.data.localId);
             dispatch(authSuccess(res.data.idToken,res.data.localId))
             dispatch(checkAuthTimeout(res.data.expiresIn))
         })
@@ -73,5 +75,23 @@ export const setAuthRedirectPath = (path) =>{
     return{
         type:actionTypes.SET_AUTH_REDIRECT_PATH,
         path:path
+    }
+}
+
+
+export const authCheckState = () =>{
+    return dispatch =>{
+        const token = localStorage.getItem('token');
+        if(!token){
+            dispatch(logout())
+        }else{
+            const expirationDate = new Date(localStorage.getItem('expirationDate'));
+            if(expirationDate < new Date()){
+                dispatch(logout())
+            }else{
+                dispatch(authSuccess(token,))
+            }
+            
+        }
     }
 }
